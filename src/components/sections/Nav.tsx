f04c9a4e102a25e18c09143874b89df341f4f00c
jasenmp <gtm4y5kghx@privaterelay.appleid.com>
2026-03-12 07:54:48 -0500

    Basic styling for nav

--- src/components/sections/Nav.tsx
+++ src/components/sections/Nav.tsx
@@ -25,11 +25,14 @@ export const NAV_ITEMS: NavItem[] = [
 
 export default function Nav() {
   return (
-    <nav aria-label="Main navigation">
-      <ul>
+    <nav
+      className="bg-surface-sunken relative mx-auto mt-6 max-w-60 rounded-md px-4 py-2"
+      aria-label="Main navigation"
+    >
+      <ul className="flex items-center justify-between">
         {NAV_ITEMS.map(({ label, href, icon: Icon }) => (
-          <li key={href}>
-            <a href={href}>
+          <li className="h-6 w-6" key={href}>
+            <a className="block" href={href}>
               <Icon aria-hidden="true" />
               <span className="sr-only">{label}</span>
             </a>
